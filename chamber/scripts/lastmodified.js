6ac4641d7d8e64001e0f46585ba8ebd1d13c226f
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-02-03 02:22:32 -0500

    changes

--- chamber/scripts/lastmodified.js
+++ chamber/scripts/lastmodified.js
@@ -1,1 +1,10 @@
 document.getElementById('lastModified').textContent = `Last Updated: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+    document.querySelector('nav ul').classList.toggle('open');
+});
