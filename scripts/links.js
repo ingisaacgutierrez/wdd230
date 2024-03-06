dfea6534575e23f3e514222d2e026e35435f4993
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-03-05 23:09:55 -0500

    link json

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,25 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
+const baseURL = "https://ingisaacgutierrez.github.io/wdd230/data/links.json";
+const linklessons = document.querySelector('#linklessons');
+
+async function getLinks() {
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+}
+
+getLinks();
+
+const displayLinks = (weeks) => {
+    let output = '<section class="card"><h3>Learning Activities</h3><ul>';
+    weeks.forEach((week) => {
+        output += `<li>${week.lesson}: `;
+        week.links.forEach((link, index) => {
+            output += `<a href="${link.url}" target="_blank">${link.title}</a>`;
+            if (index < week.links.length - 1) {
+                output += ' | ';
+            }
+        });
+        output += '</li>';
+    });
+    document.getElementById('linklessons').innerHTML = output;
+}
