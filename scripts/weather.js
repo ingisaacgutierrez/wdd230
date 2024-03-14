553e2f607772469e31926f64b5a7eaebebccb863
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-03-14 18:46:28 -0500

    update the weather in the home page

---
+++ scripts/weather.js
@@ -0,0 +1,29 @@
+const  currentTemp =  document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=2.19&lon=79.89&units=imperial&appid=8241637a5f1b31d8d03d0fbf10f496fd';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    let descCapitalized = desc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    let weatherInfo = `<div><img id="weather-icon" src="${iconsrc}" alt="${desc}"></div><div>${data.main.temp}°F - ${descCapitalized}</div>`;
+    document.querySelector('#weather').innerHTML = weatherInfo;
+}
